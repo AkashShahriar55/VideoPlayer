f50855a583e004a2088c29daaa1e3c82d147953a
akash <akashshahriar.kgs@gmail.com>
2022-06-28 19:55:56 +0600

    understanding media extractor

--- app/src/main/java/com/akash/videoplayer/MainActivity.kt
+++ app/src/main/java/com/akash/videoplayer/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.akash.videoplayer
 
 import android.media.MediaExtractor
-import android.media.session.MediaSession
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaFormat
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.MediaController
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +14,38 @@ class MainActivity : AppCompatActivity() {
 
 
         val mediaExtractor = MediaExtractor();
+        val uri = Uri.parse("android.resource://$packageName/raw/canon_in_d")
 
+        mediaExtractor.setDataSource(this,uri,null);
+
+        val numTracks = mediaExtractor.trackCount
+        Log.d("extractor", "onCreate: " + numTracks)
+
+        for(track in (0 until numTracks).withIndex()){
+            val format = mediaExtractor.getTrackFormat(track.index)
+            val mime = format.getString(MediaFormat.KEY_MIME)
+            Log.d("extractor", "onCreate: " + track.value)
+
+        }
+
+//        int numTracks = extractor.getTrackCount();
+//        for (int i = 0; i < numTracks; ++i) {
+//            MediaFormat format = extractor.getTrackFormat(i);
+//            String mime = format.getString(MediaFormat.KEY_MIME);
+//            if (weAreInterestedInThisTrack) {
+//                extractor.selectTrack(i);
+//            }
+//        }
+//        ByteBuffer inputBuffer = ByteBuffer.allocate(...)
+//        while (extractor.readSampleData(inputBuffer, ...) >= 0) {
+//            int trackIndex = extractor.getSampleTrackIndex();
+//            long presentationTimeUs = extractor.getSampleTime();
+//            ...
+//            extractor.advance();
+//        }
+//
+//        extractor.release();
+//        extractor = null;
 
 
 
